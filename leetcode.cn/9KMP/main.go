5939f150b3419c03a040d0e207d1678b2e20655e
949429516 <wklwukailun.cool@163.com>
2022-12-07 23:31:45 +0800

    kmp算法

---
+++ leetcode.cn/9KMP/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func getNext(s string) []int {
+	next := make([]int, len(s))
+	next[0] = 0
+	j := 0
+	for i := 1; i < len(s); i++ {
+		for j > 0 && s[i] != s[j] {
+			j = next[j-1]
+		}
+		if s[i] == s[j] {
+			j++
+		}
+		next[i] = j
+	}
+	return next
+}
+
+//	func getNext(s string) []int {
+//		next := make([]int, len(s))
+//		next[0] = 0
+//		for i, j := 1, 0; i < len(s); i++ {
+//			for j > 0 && s[i] != s[j] {
+//				j--
+//			}
+//			if s[i] == s[j] {
+//				next[i] = next[j] + 1
+//				j++
+//			}
+//			if j == 0 && s[i] != s[j] {
+//				next[i] = 0
+//			}
+//		}
+//		return next
+//	}
+func strStr(haystack string, needle string) int {
+	if len(haystack) == 0 || len(needle) == 0 {
+		return -1
+	}
+	next := getNext(needle)
+	j := 0
+	for i := 0; i < len(haystack); i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			j = next[j-1]
+		}
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == len(needle) {
+			return i - j + 1
+		}
+	}
+	return -1
+}
+func main() {
+	fmt.Println(strStr("aabaabaafa", "aabaaf"))
+}
